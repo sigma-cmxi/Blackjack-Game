b6b5afa747c0a479f013e920e1bc62b0f77931ef
sigma-cmxi <sigma.cmxi@gmail.com>
2024-11-20 14:59:59 +0000

    day - 5

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 let firstCard = Math.floor(Math.random() * 10) + 2
 let secondCard = Math.floor(Math.random() * 10) + 2
+let sum = 21
 
-let sum = firstCard + secondCard
+let message;
+let hasBlackJack = false
+let isAlive = true
+
+function startGame() {
+    
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    console.log(message)
+}
